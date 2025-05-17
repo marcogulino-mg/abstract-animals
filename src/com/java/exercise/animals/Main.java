543b368b5834291d645f6d43df9c6132836c70df
marcogulino-mg <marcogulino98@gmail.com>
2025-05-17 18:07:04 +0200

    feat:   Main.java:     - test of Dog instances and inherited Methods from AbstractAnimal

--- src/com/java/exercise/animals/Main.java
+++ src/com/java/exercise/animals/Main.java
@@ -1,7 +1,23 @@
 package com.java.exercise.animals;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+        /*
+         * //DEBUG: Test of Dog instances and Abstract Methods inherited from
+         * //DEBUG: AbstractAnimal
+         */
+        // Context: Instances of Dog Class
+        Dog emptyDog = new Dog();
+        Dog newDog = new Dog(5, BigDecimal.valueOf(30.40), "Pastore Tedesco", "Marrone", "Urbano", "Large");
 
+        // Context: invoke Methods of the newDog Object
+        System.out.println(newDog);
+        newDog.mangia();
+        newDog.verso();
+
+        // Context: invoke Methods of the emptyDog Object
+        System.out.println(emptyDog);
     }
 }
