a4afe9414dfb5db62779067911ab07a948ec58bc
marcogulino-mg <marcogulino98@gmail.com>
2025-05-17 16:43:20 +0200

    feat:   - add AbstractAnimal Class   AbstractAnimal:     - add Concrete/Abstract Methods

---
+++ src/com/java/exercise/animals/AbstactAnimal.java
@@ -0,0 +1,14 @@
+package com.java.exercise.animals;
+
+public abstract class AbstactAnimal {
+    // Context: Methods
+    // Context: Concrete Methods
+    public void dormi() {
+        System.out.println("Zzz....");
+    }
+
+    // Context: Abstract Methods
+    public abstract void verso();
+
+    public abstract void mangia();
+}
